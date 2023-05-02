0a8263306cc0e4cd3fa68a607de3db0100e165af
Matheus de Mello <matheusmb2003@gmail.com>
2023-05-02 18:57:29 -0300

    Alterações classes

--- src/App.java
+++ src/App.java
@@ -1,20 +1,36 @@
+import java.util.List;
+import java.util.Scanner;
 import model.Dados;
 import model.Itinerario;
+import model.Passagem;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Dados.getItinerarios().add(new Itinerario("São Paulo", "Rio de Janeiro", "01/01/2021"));
+        Itinerario itinerario = new Itinerario("São Paulo", "Rio de Janeiro", "01/01/2022");
+        List<Passagem> passagens = itinerario.getPassagens();
+        System.out.println(passagens);
 
-        System.out.printf("Origem original:" + Dados.getItinerarios().get(0).getOrigem() + "\n");
+        //Adiciona o itinerário na lista de itinerários na classe Dados
+        Dados.getItinerarios().add(itinerario);
+
+        System.out.println("Origem incial: " + Dados.getItinerarios().get(0).getOrigem());
+
+        Scanner scanner = new Scanner(System.in); // criar um objeto Scanner
 
         int posicaoItinerario = 0; // armazena a posição do itinerário na lista
-        Dados.getItinerarios().get(posicaoItinerario).setOrigem("Brasilia"); // utiliza a posição para alterar a origem
+        
+        // Solicitar ao usuário que insira uma nova origem
+        System.out.println("Digite a nova origem: ");
+        String novaOrigem = scanner.nextLine();
 
-        System.out.printf("Origem alterada:" + Dados.getItinerarios().get(posicaoItinerario).getOrigem() + "\n");
+        // utilizar a posição para alterar a origem
+        Dados.getItinerarios().get(posicaoItinerario).setOrigem(novaOrigem);
+        System.out.println("Origem alterada: " + Dados.getItinerarios().get(posicaoItinerario).getOrigem());
 
-        System.out.printf("Tamanho antes de remover:" + Dados.getItinerarios().size() + "\n");
+        System.out.println("Tamanho antes de remover: " + Dados.getItinerarios().size());
         Dados.getItinerarios().remove(posicaoItinerario);
-        System.out.printf("Tamanho depois de remover:" + Dados.getItinerarios().size() + "\n");
+        System.out.println("Tamanho depois de remover: " + Dados.getItinerarios().size());
 
+        scanner.close(); // fechar o objeto Scanner
     }
 }
